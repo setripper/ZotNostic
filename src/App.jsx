b01c84e4b178ac8b2ca28bd2ffa5f863b89b4567
Edison Trinh <panteat@uci.edu>
2024-11-20 04:20:25 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import leftSide from './components/leftSide/leftSide'
+
+const App = () => {
+  return (
+    <>
+      <leftSide/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/leftSide/leftSide.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './leftSide.css'
+
+const leftSide = () => {
+  return (
+    <div className = 'leftSide'>
+
+        <div className="top">
+
+        </div>
+
+        <div className="bottom">
+
+        </div>
+
+    </div>
+  )
+}
+
+export default leftSide
