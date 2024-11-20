9de25bcdfa731c6891e38d64b9c17ce4e3bacc73
Edison Trinh <panteat@uci.edu>
2024-11-20 04:31:17 -0800

    quick change

---
+++ src/components/rightSide/rightSide.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function rightSide() {
+  return (
+    <div className="rightSide">
+
+        <div className="top">
+
+        </div>
+
+        <div className="bottom">
+
+        </div>
+        
+    </div>
+  )
+}
+
+export default rightSide
