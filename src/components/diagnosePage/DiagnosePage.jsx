8d7beedb529b0b8fae716a03d2e41aee06a077bf
Edison Trinh <trinhem@uci.edu>
2024-11-22 00:27:11 -0800

    testing for pranav

--- src/components/diagnosePage/DiagnosePage.jsx
+++ src/components/diagnosePage/DiagnosePage.jsx
@@ -5,6 +5,7 @@ import SideBar from '../sideBar/SideBar'
 function DiagnosePage() {
   return (
     <div className='Diagnose'>
+        <h1> PRANAV IS STUPID AS HELL</h1>
         <SideBar/>
     </div>
   )
