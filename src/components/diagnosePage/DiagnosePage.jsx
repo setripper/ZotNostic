6671ade57cf1279f4c9cef521c34f3aa2c2ab40a
Pradosh Thirunavukkarasu <tpradosh360@gmail.com>
2024-11-22 04:17:21 -0800

    diagnose page prompt

--- src/components/diagnosePage/DiagnosePage.jsx
+++ src/components/diagnosePage/DiagnosePage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './DiagnosePage.css'
 import SideBar from '../sideBar/SideBar'
 //import { Context } from '../../backend/Context'
+//import send_icon from './send_icon.png'
 
 
 const DiagnosePage = () => {
@@ -10,10 +11,58 @@ const DiagnosePage = () => {
 
 
   return (
-    <div className='Diagnose'> 
-        <SideBar/>
+    <>
+    <SideBar/>
+    <div className='Diagnose'>
+        <div className = "nav">
+          <p>ZOTNostic</p>
+        </div>
 
-    </div>
+      <div className = "main-container">
+        <div className = "greet">
+          <p><span>Hello, NAMETOFILLIN </span></p>
+          <p>How can I help you</p>
+        </div>
+
+        <div className = "cards">
+          <div className="card">
+            <p>Suggest a healthy meal to eat while sick</p>
+          </div>
+          <div className="card">
+            <p>Recommed me a nearby doctor to see</p>
+          </div>
+          <div className="card">
+            <p>Suggest tips on how to recover quickly</p>
+          </div>
+          <div className="card">
+            <p>Suggest a healthy meal to eat while sick</p>
+          </div>
+        </div>
+
+      </div>
+
+      <div className = "main-bottom">
+        <div className="searchbox">
+          <input type ="text" placeholder = "enter your symptoms"></input>
+          <div>
+            
+          </div>
+        </div>
+
+        <p className="bottom-info">
+          Disclaimer tag about the information we type in
+        </p>
+
+      </div>
+
+
+
+
+
+
+
+    </div> 
+    </>
   )
 }
 
