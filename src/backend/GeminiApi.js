cbd4e67a4d328fd45c1a6e5c31fbd732eb7cfc62
Pradosh Thirunavukkarasu <tpradosh360@gmail.com>
2024-11-22 16:09:22 -0800

    slight changes

--- src/backend/GeminiApi.js
+++ src/backend/GeminiApi.js
@@ -1,18 +1,16 @@
 
 
 import { GoogleGenerativeAI } from "@google/generative-ai";
-const API_KEY = 'API_KEY = AIzaSyC_w8xas9oJR6v8EEH5OOqvvWiysNoSY2s'
 
 
 async function call_gemini(input) {
+    const API_KEY = 'AIzaSyC_w8xas9oJR6v8EEH5OOqvvWiysNoSY2s';
 
     const genAI = new GoogleGenerativeAI(API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
     const prompt = `Context: you are a doctor, and your job is to diagnose me 
-    with a few possible illness i may have given the symptoms: ${input}. When sending 
-    your response back, only return a list of possible illness, no questions and no other 
-    filler text`
+    with a few possible illness i may have given the symptoms: ${input}. `
 
 
     const result = await model.generateContent(prompt);
